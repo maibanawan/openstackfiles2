f6d0e16c235f7ff988d345dccce40d4e5562fba5
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-05 19:18:56 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#import sys
+#arg1=sys.argv[1]
 
 export RESP_JSON_AUTH=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens)
 export API_TOKEN=$(echo $RESP_JSON_AUTH) 
 echo $API_TOKEN
+echo $arg1
