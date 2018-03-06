b6e7ebc1f8b5f0d1ec3d4a88cc0e9a051a96d70a
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-05 19:09:40 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 export RESP_JSON_AUTH=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens)
 export API_TOKEN=$(echo $RESP_JSON_AUTH) 
-echo "test2"
+echo "test2 after deleting"
