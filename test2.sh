60173b0bf11d2ba6893b41f896bdd25e83ce531f
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-05 19:14:06 -0500

    Create test2.sh

---
+++ test2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export RESP_JSON_AUTH=$(curl -i -H "Content-Type: application/json" -d '{ "auth": {"identity": {"methods": ["password"],"password": {"user": {"name": "admin","domain": { "id": "default" },"password": "secret"}}},"scope": {"project": {"name": "admin","domain": { "id": "default" }}}}}' http://localhost/identity/v3/auth/tokens)
+export API_TOKEN=$(echo $RESP_JSON_AUTH'.access.token.id') 
